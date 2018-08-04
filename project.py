327a19ce9161086e77302096bf0408e12476b904
Abdullah <noman19bd@yahoo.com>
2018-08-04 07:23:34 +0600

    Updated Data Mining Project

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import requests
+import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
@@ -22,9 +23,20 @@ population_list = []
 
 for i, col_one in enumerate(new_body[:]):
     col_one = new_body[i].text.replace(',', '')
-    population_list.append(col_one)
+    population_list.append(int(col_one))
 
 print(population_list)
+
+
+# Getting differences from this bangladesh population list
+diff_BD = []
+
+for i, val in enumerate(population_list[:15]):
+    light = val - population_list[i + 1]
+    diff_BD.append(light)
+
+print(diff_BD)
+
 
 # Here I'm getting Years
 for_get_year = temp.find_all("td")
@@ -36,7 +48,7 @@ year_list = []
 for j in range(0, 208, 13):
     for zero in enumerate(for_get_year):
         zero = for_get_year[j].text.replace(',', '')
-    year_list.append(zero)
+    year_list.append(int(zero))
 
 print(year_list)
 
@@ -55,26 +67,80 @@ for k in range(6, 214, 13):
 print(growth_list)
 
 
+# Here I'm getting World Population
+get_world = temp.find_all("td")
+
+world_population = []
+
+# This Loop is for clear noise and get all growth rate from that list : Growth Rate
+
+for k in range(11, 219, 13):
+    for world in enumerate(get_world):
+        world = get_world[k].text.replace(',', '')
+    world_population.append(int(world))
+
+print(world_population)
+
+# Getting differences from this bangladesh population list
+diff_world = []
+
+for i, value in enumerate(world_population[:15]):
+    tube = value - world_population[i + 1]
+    diff_world.append(tube)
+
+print(diff_world)
+
+
+print("Difference Between World Population and Bangladesh Population")
+difference = [a - b for a, b in zip(world_population, population_list)]
+print(difference)
+
+print('\n')
+
 # This Data Frame is for create a perfect view table
 
 population = pd.DataFrame({
         "Year": year_list,
         "Population": population_list,
-        "Growth": growth_list
+        "Growth": growth_list,
+        "World": world_population,
+        "Difference": difference
 
     })
 
 print(population)
 
+print('\n')
+
+# This Data Frame is for difference view
+
+diff = pd.DataFrame({
+        "Diff-BD": diff_BD,
+        "Diff-World": diff_world
+
+    })
+
+print(diff)
+
 # Here I'm reverse my data list for low to high view in Python Visualization
 year_list.reverse()
 population_list.reverse()
 growth_list.reverse()
+world_population.reverse()
+difference.reverse()
+diff_BD.reverse()
+diff_world.reverse()
+
 
 # After Reverse then I'm insert into new list
 x = year_list
 y = population_list
 z = growth_list
+w = world_population
+d_bd = diff_BD
+d_world = diff_world
+d = difference
+
 
 # Making Plot as well as Scatter for Years with Populations
 plt.plot(x, y)
@@ -96,4 +162,11 @@ plt.xlabel("Year")
 plt.ylabel("Growth Rate")
 
 plt.legend(["Growth Plot", "Growth Scatter"])
+plt.show()
+
+
+plt.hist([d, w], color=['red', 'green'], bins=25)
+plt.title('Comparison :: Bangladesh Vs World Population')
+plt.legend(["Bangladesh Population", "World Population"])
+
 plt.show()
