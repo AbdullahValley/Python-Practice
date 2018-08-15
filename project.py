0b34bcb5a31cd3033d3b3216a53b9dca215208dc
Abdullah <noman19bd@yahoo.com>
2018-08-15 08:44:19 +0600

    Pi Chart Added and Bar Chart Updated

--- project.py
+++ project.py
@@ -122,7 +122,9 @@ diff = pd.DataFrame({
 
 print(diff)
 
+
 # Here I'm reverse my data list for low to high view in Python Visualization
+
 year_list.reverse()
 population_list.reverse()
 growth_list.reverse()
@@ -141,6 +143,15 @@ d_bd = diff_BD
 d_world = diff_world
 d = difference
 
+'''
+
+plt.hist([d, w], color=['red', 'green'], bins=25)
+plt.title('Comparison :: Bangladesh Vs World Population')
+plt.legend(["Bangladesh Population", "World Population"])
+
+plt.show()
+
+'''
 
 # Making Plot as well as Scatter for Years with Populations
 plt.plot(x, y)
@@ -165,30 +176,58 @@ plt.legend(["Growth Plot", "Growth Scatter"])
 plt.show()
 
 
-plt.hist([d, w], color=['red', 'green'], bins=25)
-plt.title('Comparison :: Bangladesh Vs World Population')
-plt.legend(["Bangladesh Population", "World Population"])
+# Bar Chart Start -------------->
+
+N = 10
+ind = np.arange(N)
+width = 0.25
+fig, ax = plt.subplots()
+
+rects1 = ax.bar(ind, d_bd[0:10], width, color='g')
+
+rects2 = ax.bar(ind + width, d_world[0:10], width, color='r')
+
+
+ax.set_xlabel('Year')
+ax.set_ylabel('Population')
+ax.set_title('Difference between Bangladesh and World Populations')
+ax.set_xticks(ind + width / 2)
+ax.set_xticklabels(('2018-2017', '2017-2016', '2016-2015', '2015-2010', '2010-2005', '2005-2000', '2000-1995', '1995-1990','1990-1985', '1985-1980'))
+
+ax.legend(('Bangladesh', 'World'))
 
 plt.show()
 
-# Bar
+# Bar Chart END ------------->
 
-N = 5
-menMeans = (20, 35, 30, 35, 27)
-womenMeans = (25, 32, 34, 20, 25)
-menStd = (2, 3, 4, 1, 2)
-womenStd = (3, 5, 2, 3, 3)
-ind = np.arange(N)    # the x locations for the groups
-width = 0.35       # the width of the bars: can also be len(x) sequence
 
-p1 = plt.bar(ind, menMeans, width, yerr=menStd)
-p2 = plt.bar(ind, womenMeans, width,
-             bottom=menMeans, yerr=womenStd)
+# Pie Chart Start -------------->
 
-plt.ylabel('Scores')
-plt.title('Scores by group and gender')
-plt.xticks(ind, ('G1', 'G2', 'G3', 'G4', 'G5'))
-plt.yticks(np.arange(0, 81, 10))
-plt.legend((p1[0], p2[0]), ('Men', 'Women'))
+def get_change(current, previous):
+    if current == previous:
+        return 100.0
+    try:
+        return (abs(current - previous) / previous) * 100.0
+    except ZeroDivisionError:
+        return 0
+
+
+bd = get_change(d_bd[0], d_bd[1])
+
+world = get_change(d_world[0], d_world[1])
+
+# print(bd)
+# print(world)
+
+labels = 'Bangladesh', 'World'
+ratio = [bd, world]
+
+fig1, ax1 = plt.subplots()
+
+ax1.pie(ratio, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+ax1.axis('equal')
+ax1.set_title("Last Iteration (2018-2017) Changing Ratio")
 
 plt.show()
+
+# Pie Chart END -------------->
